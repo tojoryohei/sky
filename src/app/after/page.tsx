8ac843ed5c42cc6239cf86a95ed59d07c15aeb08
tojoryohei <tojoryohei@yahoo.co.jp>
2024-08-07 23:47:42 +0900

    error

---
+++ src/app/after/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <>
+            <h1>遷移後</h1>
+        </>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,38 @@
-import Image from "next/image";
+import { Textarea, FormControl, FormLabel, FormErrorMessage, Button } from "@chakra-ui/react";
+import React from "react";
+import { useForm } from "react-hook-form";
+
+interface Inputs {
+  textarea: string
+}
 
 export default function Home() {
+  // useFormフックの初期化
+  const { register, handleSubmit, formState: { errors } } = useForm<Inputs>();
+
+  // フォーム送信時の処理
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
   return (
     <>
-      <h1>a</h1>
+      <h1>必須項目</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormControl isInvalid={!!errors.textarea}>
+          <Textarea
+            id="textarea"
+            placeholder="入力してください"
+            {...register("textarea", { required: "入力してください" })}
+          />
+          <FormErrorMessage>
+            {!!errors.textarea && !!errors.textarea.message}
+          </FormErrorMessage>
+        </FormControl>
+        <Button colorScheme="blue">
+          出力値を確認
+        </Button>
+      </form>
     </>
   );
 }
